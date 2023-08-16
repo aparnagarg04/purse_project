e7f69cb8846246bd356822b1b3eabe11e092f4a6
Aparna garg <aparnagargstg04@gmail.com>
2023-08-16 14:46:09 +0530

    added code on git

---
+++ src/screens/MacbookPro/MacbookPro.jsx
@@ -0,0 +1,181 @@
+import React from "react";
+import "./style.css";
+
+export const MacbookPro = () => {
+  return (
+    <div className="macbook-pro">
+      <div className="div">
+        <div className="overlap">
+          <div className="group">
+            <div className="text-wrapper">FASHION &amp; ACCESSORIES</div>
+            <div className="FRAGRANCE-BEAUTY">FRAGRANCE &amp; BEAUTY</div>
+          </div>
+          <img
+            className="image"
+            alt="Image"
+            src="https://generation-sessions.s3.amazonaws.com/54bdc6a0695d6f02a8853c8e4f0ecd46/img/image-30-4@2x.png"
+          />
+          <img
+            className="tabler-search"
+            alt="Tabler search"
+            src="https://generation-sessions.s3.amazonaws.com/54bdc6a0695d6f02a8853c8e4f0ecd46/img/tabler-search.svg"
+          />
+        </div>
+        <div className="navbar">
+          <div className="text-wrapper-2">WHAT’S NEW</div>
+          <div className="text-wrapper-3">GIFTS</div>
+          <div className="text-wrapper-4">WOMEN’S FASHION</div>
+          <div className="text-wrapper-5">MEN’S FASHION</div>
+          <div className="text-wrapper-6">BAGS</div>
+          <div className="KIDS-BABY">KIDS &amp; BABY</div>
+          <div className="text-wrapper-7">HOME</div>
+          <div className="text-wrapper-8">FASHION SHOWS</div>
+          <div className="text-wrapper-9">DIOR WORLD</div>
+        </div>
+        <div className="side-view">
+          <img
+            className="img"
+            alt="Image"
+            src="https://generation-sessions.s3.amazonaws.com/54bdc6a0695d6f02a8853c8e4f0ecd46/img/image-31-1@2x.png"
+          />
+        </div>
+        <div className="frame">
+          <img
+            className="image-2"
+            alt="Image"
+            src="https://generation-sessions.s3.amazonaws.com/54bdc6a0695d6f02a8853c8e4f0ecd46/img/image-30-3@2x.png"
+          />
+        </div>
+        <div className="frame-2" />
+        <div className="overlap-group">
+          <div className="overlap-wrapper">
+            <div className="overlap-2">
+              <div className="AR">View in AR</div>
+              <img
+                className="vector"
+                alt="Vector"
+                src="https://generation-sessions.s3.amazonaws.com/54bdc6a0695d6f02a8853c8e4f0ecd46/img/vector-1.svg"
+              />
+              <img
+                className="material-symbols"
+                alt="Material symbols"
+                src="https://generation-sessions.s3.amazonaws.com/54bdc6a0695d6f02a8853c8e4f0ecd46/img/material-symbols-view-in-ar-outline-1.svg"
+              />
+            </div>
+          </div>
+          <div className="div-wrapper">
+            <div className="text-wrapper-10">View in 3D</div>
+          </div>
+        </div>
+        <div className="image-wrapper">
+          <img
+            className="image-3"
+            alt="Image"
+            src="https://generation-sessions.s3.amazonaws.com/54bdc6a0695d6f02a8853c8e4f0ecd46/img/image-30-2@2x.png"
+          />
+        </div>
+        <img
+          className="line"
+          alt="Line"
+          src="https://generation-sessions.s3.amazonaws.com/54bdc6a0695d6f02a8853c8e4f0ecd46/img/line-1.svg"
+        />
+        <div className="text-wrapper-11">NEW</div>
+        <div className="overlap-3">
+          <div className="frame-3">
+            <div className="text-wrapper-12">COLOR AVAILABLE</div>
+            <div className="frame-4" />
+            <div className="img-wrapper">
+              <img
+                className="image-4"
+                alt="Image"
+                src="https://generation-sessions.s3.amazonaws.com/54bdc6a0695d6f02a8853c8e4f0ecd46/img/image-30-1@2x.png"
+              />
+            </div>
+            <div className="frame-5">
+              <img
+                className="image-5"
+                alt="Image"
+                src="https://generation-sessions.s3.amazonaws.com/54bdc6a0695d6f02a8853c8e4f0ecd46/img/image-30@2x.png"
+              />
+            </div>
+            <div className="frame-6">
+              <img
+                className="image-6"
+                alt="Image"
+                src="https://generation-sessions.s3.amazonaws.com/54bdc6a0695d6f02a8853c8e4f0ecd46/img/image-16@2x.png"
+              />
+            </div>
+          </div>
+          <img
+            className="image-7"
+            alt="Image"
+            src="https://generation-sessions.s3.amazonaws.com/54bdc6a0695d6f02a8853c8e4f0ecd46/img/image-31@2x.png"
+          />
+        </div>
+        <div className="text-wrapper-13">$ 3,000</div>
+        <div className="text-wrapper-14">Save 25%</div>
+        <div className="text-wrapper-15">MINI LADY DIOR BAG</div>
+        <div className="overlap-group-wrapper">
+          <div className="overlap-group-2">
+            <div className="text-wrapper">$ ,3999</div>
+            <img
+              className="line-2"
+              alt="Line"
+              src="https://generation-sessions.s3.amazonaws.com/54bdc6a0695d6f02a8853c8e4f0ecd46/img/line-2.svg"
+            />
+          </div>
+        </div>
+        <div className="frame-7">
+          <div className="text-wrapper-16">ADD TO CART</div>
+        </div>
+        <div className="overlap-4">
+          <div className="frame-8">
+            <div className="text-wrapper-17">DESCRIPTION</div>
+            <p className="elegant-lady-dior">
+              Elegant Lady Dior Bag
+              <br />
+              Timeless Quilted Design
+              <br />
+              Beige Lambskin Material
+              <br />
+              Removable Chain Strap
+              <br />
+              Crossbody or Handheld
+              <br />
+              D.I.O.R. Charm Accents
+              <br />
+              Interior Zip Pocket
+              <br />
+              Embroidered Strap Compatibility
+              <br />
+              Dust Bag Included
+              <br />
+              Italian Craftsmanship
+            </p>
+            <img
+              className="mdi-minus"
+              alt="Mdi minus"
+              src="https://generation-sessions.s3.amazonaws.com/54bdc6a0695d6f02a8853c8e4f0ecd46/img/mdi-minus.svg"
+            />
+          </div>
+          <div className="text-wrapper-18">Mini Lady Dior Bag</div>
+        </div>
+        <div className="ps-chat-alt-wrapper">
+          <img
+            className="ps-chat-alt"
+            alt="Ps chat alt"
+            src="https://generation-sessions.s3.amazonaws.com/54bdc6a0695d6f02a8853c8e4f0ecd46/img/ps-chat-alt.svg"
+          />
+        </div>
+        <div className="frame-9">
+          <div className="text-wrapper-19">DELIVERY AND RETURN</div>
+          <img
+            className="ic-outline-plus"
+            alt="Ic outline plus"
+            src="https://generation-sessions.s3.amazonaws.com/54bdc6a0695d6f02a8853c8e4f0ecd46/img/ic-outline-plus.svg"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
